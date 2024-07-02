95549dee9e7b579328cc1372752c63def2fadd4f
keigo <g22307@kagawa.kosen-ac.jp>
2024-07-02 16:44:43 +0900

    初期コミット

---
+++ README.md
@@ -0,0 +1,3 @@
+# mobile
+
+U22 mobile flutter projecy
---
+++ android/app/src/main/kotlin/jp/kosen_ac/kagawa/mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package jp.kosen_ac.kagawa.mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
