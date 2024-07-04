b37d9e5cb4af6810fa6d518f80fcfb51e4838043
keigo <89068705+gongitsune@users.noreply.github.com>
2024-07-04 19:35:21 +0900

    WidgetsFlutterBindingの初期化時に実行

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:mobile/firebase_options.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
